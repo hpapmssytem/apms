5a13b5335cac0f20aefa73d4d95c755ece53ab31
unknown <staff01-hubport@Computer-048-PC.hpi.local>
2015-09-29 09:53:42 +0800

    Refined code

--- resources/views/positions/create.blade.php
+++ resources/views/positions/create.blade.php
@@ -11,9 +11,8 @@
         </div>
     </div>
 	<div class="row">
-		<div class="col-lg-12">
-			{!! Form::open(array('route' => 'positions.store', 'class' => 'form')) !!}
-
+		{!! Form::open(array('route' => 'positions.store', 'class' => 'form')) !!}
+			<div class="col-lg-12 floating-label-form-group controls">
 				<div class="form-group">
 					<h5>Availability</h5>
 					<select name="status">
@@ -22,7 +21,7 @@
 					</select>
 				</div>
 
-				<div class="form-group">
+				<div class="control-group">
 					<h5>Position Name</h5>
 					{!! Form::text('name', null,
 						array('required', 'class' => 'form-control')) !!}
@@ -32,13 +31,11 @@
 					<h5>Description</h5>
 					{!! Form::textarea('description', null,
 						array('required', 'class' => 'form-control')) !!}
-				</div>
-
-				<div class="form-group">
-					{!! Form::submit('Create Position', array('class'=>'btn btn-success btn-md')) !!}
-				</div>
-
-			{!! Form::close() !!}	
-		</div>
+				</div>	
+			</div>
+			<div class="col-lg-12 form-group">
+				{!! Form::submit('Create Position', array('class'=>'btn btn-success btn-md')) !!}
+			</div>
+		{!! Form::close() !!}
 	</div>
 @endsection
