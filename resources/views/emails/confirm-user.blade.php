2276214e643dfa50141ee039a7ca97fea4ea9279
unknown <staff01-hubport@Computer-048-PC.hpi.local>
2015-10-09 11:07:21 +0800

    added confirmation letter to user

---
+++ resources/views/emails/confirm-user.blade.php
@@ -0,0 +1,12 @@
+<h2>Your account has been authorized</h2>
+
+
+<p>Hello {{ $name }},</p>
+
+<div>
+    Thank you for registering to APMS. You can login to your account
+    using your email address and password. Please make your credentials
+    discrete.
+    <br /><br />
+    God Bless! 
+</div>
